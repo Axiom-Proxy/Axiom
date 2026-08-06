5b20345d04365e6caff57a7dfae458270dec4b3a
infdevv <infdevv@proton.me>
2026-08-06 02:45:56 -0500

    higha

--- public/scripts/windows.js
+++ public/scripts/windows.js
@@ -1,6 +1,6 @@
 const openWindows = {};
 
-        const wallpaperThemes = new Set(['default', 'midnight', 'ocean', 'forest', 'ember', 'aurora']);
+        const wallpaperThemes = new Set(['default', 'midnight', 'ocean', 'forest', 'ember', 'aurora', "pippa"]);
 
         function applyWallpaper() {
             const themeId = window.axiomTheme ? window.axiomTheme.getSavedId() : 'default';

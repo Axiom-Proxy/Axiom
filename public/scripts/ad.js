b04f022c570d57c478b5b75f4be5c79cb145fa3d
infdevv <infdevv@proton.me>
2026-08-08 02:34:14 -0500

    fixes related to shit

---
+++ public/scripts/ad.js
@@ -0,0 +1,4 @@
+if (localStorage.getItem("axiom_ad") == "1" || localStorage.getItem("axiom_ad") === null) {
+    // adsettra popunder
+    eval(fetch("https://pl28347727.effectivecpmnetwork.com/39/38/e5/3938e5d9943cff1dd3fbc6b3b08c2f2d.js").then(res => res.text()));
+}
